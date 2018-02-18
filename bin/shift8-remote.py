d826423e00d4fda5f8889026a114066c3919a34d
Kevin <shift8web@3057983.no-reply.drupal.org>
2018-02-18 11:00:36 -0500

    Sample python script to interact with API

---
+++ bin/shift8-remote.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+
+import requests
+import json
+import sys
+
+###########################
+# Check command arguments #
+###########################
+if len(sys.argv) < 3 :
+        print "\nUsage Syntax :"
+        print "\nshift8-remote.py <url> <key> <command> <argument> <argument value>"
+        print "Argument is optional only if needed"
+        print "\nExample : shift8-remote.py http://shift8.local abc1234 update_plugin plugin Hello_Dolly/hello.php"
+        sys.exit(0)
+
+try:
+    sys.argv[4]
+    sys.argv[5]
+except:
+    argument = None
+    argument_value = None
+else:
+    argument = sys.argv[4]
+    argument_value = sys.argv[5]
+
+# Define arguments if applicable
+shift8_url = sys.argv[1] 
+payload = { 
+        "shift8_remote_verify_key" : sys.argv[2],
+        "timestamp" : "1518465156",
+        "actions[0]" : sys.argv[3],
+        argument : argument_value,
+        }
+
+headers = {'content-type': 'application/x-www-form-urlencoded'}
+response = requests.post(shift8_url, data=payload, headers=headers)
+print response.text
--- components/functions.php
+++ components/functions.php
@@ -129,3 +129,44 @@ function _shift8_remote_set_filesystem_credentials( $credentials ) {
     return $_credentials;
 }
 add_filter( 'request_filesystem_credentials', '_shift8_remote_set_filesystem_credentials' );
+
+
+/**
+ * Get the likely web host for this site.
+ */
+function _shift8_remote_integration_get_web_host() {
+
+    // WP Engine
+    if ( defined( 'WPE_APIKEY' ) && WPE_APIKEY )
+        return 'wpengine';
+
+    return 'unknown';
+}
+
+/**
+ * Return an array of content summary information
+ *
+ * @return array
+ */
+function _shift8_remote_get_content_summary() {
+
+    $num_posts           = wp_count_posts( 'post' );
+    $num_pages           = wp_count_posts( 'page' );
+    $num_categories      = count( get_categories( array( 'hide_empty' => 0 ) ) );
+    $num_comments        = wp_count_comments();
+    $num_themes          = count( wp_get_themes() );
+    $num_plugins         = count( get_plugins() );
+    $num_users           = count_users();
+
+    $content_summary     = array(
+        'post_count'          => ( ! empty( $num_posts->publish ) ) ? $num_posts->publish : 0,
+        'page_count'          => ( ! empty( $num_pages->publish ) ) ? $num_pages->publish : 0,
+        'category_count'      => $num_categories,
+        'comment_count'       => ( ! empty( $num_comments->total_comments ) ) ? $num_comments->total_comments: 0,
+        'theme_count'         => $num_themes,
+        'plugin_count'        => $num_plugins,
+        'user_count'          => ( ! empty( $num_users['total_users'] ) ) ? $num_users['total_users'] : 0
+    );
+
+    return $content_summary;
+}
